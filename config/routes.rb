c6266cfb9127a624cdbe68d779884bd4461274c9
yuna <yuna0702@gmail.com>
2017-02-27 13:01:50 +0000

    create messages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+end
+Rails.application.routes.draw do
+  get 'messages/index'
+end
+Rails.application.routes.draw do
+ root 'messages#index'
 end
--- db/migrate/20170224234714_create_messages.rb
+++ db/migrate/20170224234714_create_messages.rb
@@ -2,7 +2,17 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.string :name
-      t.body :body
+      t.string :body
+
+      t.timestamps null: false
+    end
+  end
+end
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :name
+      t.string :body
 
       t.timestamps null: false
     end
