8b1f0f9d05b5e7c917b41ed43c70d85925303b0f
yuna <yuna0702@gmail.com>
2017-02-27 13:36:06 +0000

    create message model

--- db/migrate/20170224234714_create_messages.rb
+++ db/migrate/20170224234714_create_messages.rb
@@ -8,13 +8,4 @@ class CreateMessages < ActiveRecord::Migration
     end
   end
 end
-class CreateMessages < ActiveRecord::Migration
-  def change
-    create_table :messages do |t|
-      t.string :name
-      t.string :body
 
-      t.timestamps null: false
-    end
-  end
-end
