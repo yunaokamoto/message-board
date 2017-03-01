a6d3025330f052171efb203145a20b2597c25ea6
yuna <yuna0702@gmail.com>
2017-03-01 15:55:23 +0000

    first commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,7 @@ class MessagesController < ApplicationController
     @messages = Message.all
 end
 
-def create
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
-    else
-      # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-    end
-  end
+
 
   private
   def message_params
