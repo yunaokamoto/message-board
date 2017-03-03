f70b783beef61b23f4b00a865aedc5cb3d6d8031
yuna <yuna0702@gmail.com>
2017-03-03 10:23:38 +0000

    age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,7 @@ def create
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :age, :body)
   end
   
   def set_message
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
      # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
+     # 年齢は0以上かつ150以下の数字
+  validates :age , numericality: { minimum:0, maximum:150}, presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
