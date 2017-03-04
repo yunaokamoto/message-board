3e6d7fd16de42c6ead62cf79fef13f638c5953eb
yuna <yuna0702@gmail.com>
2017-03-04 08:25:32 +0000

    Gemfile update for heroku

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
      # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
      # 年齢は0以上かつ150以下の数字
-  validates :age , numericality: { minimum:0, maximum:150}, presence: true
+  validates :age , numericality:{ only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
